a29c98316769e137c4a9cd8abaf338ce7882cb1b
gguzman8 <gguzman8@ucol.mx>
2023-11-29 15:57:25 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// let video = document.querySelector(".botonVideo");
+
+// video.addEventListener("play", function (){
+//     console.log("Di play al video")
+// })
+
+// video.addEventListener("seeking", function (){
+//     console.log("Se esta viendo una parte del video", this.currentTime );
+// })
+
+// video.addEventListener("ended", function (){
+//     console.log("Termino el video")
+//     let decision = confirm("¿Deseas ver de nuevo el video?");
+//     console.log(decision);
+//     if (decision){
+//         video.play();
+//     } else { 
+//         window.location = "https://www.google.com"
+//     }
+// })
+
+// video.addEventListener("pause", function (){
+//     console.log("Puso pausa en el video")
+// })
+
+// Temporizadores o timers
+
+// setInterval
+// setTimeout
+
+
+// function ponerColorBody(){
+//     let pantalla = document.body;
+//     pantalla.style.background = 
+//     pantalla.style.background == "blue" ? "green" : "blue"
+//     }
+
+// let tempozador = setInterval(function(){
+// ponercolor();
+// },2000)
+// function ponercolor(){
+//     let pantalla = document.body;
+//     pantalla.style.backgroundColor = pantalla.style.backgroundColor == "blue" ? "purple" : "blue";
+// }
+
+ARREGLOS, VECTORES, ARRAYS
+
+let array = ['uno', 'dos', 'tres'];
+
+let array2 = ['cuatro', 'cinco', 'seis'];
+
+let multidimencional = [array, array2]
